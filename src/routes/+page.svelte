dd01ad611851dc6519b3598380209bd2a67fb3a6
JPEG <139462407+jpeg-artifact@users.noreply.github.com>
2025-03-17 10:48:16 +0100

    feat: add table texture

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+    import Comp from "$lib/Comp.svelte";
+
+    let bo : string = $state("1.1")
+    let reber : string = "bo"
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h3 class="w-1/2 text-right text-xl font-bold">{#if bo === "1.1"} meow meow {/if}</h3>
+<input type="text" bind:value={bo}>
+<Comp></Comp>
