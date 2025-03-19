d61a3ece798dd9392ed6a1aec95b742ae0551361
JPEG <139462407+jpeg-artifact@users.noreply.github.com>
2025-03-19 16:15:28 +0100

    feat: add card selection

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,7 +1,11 @@
 <script>
-    
+    let { number, selected = $bindable() } = $props();
+
+    function onclick() {
+        selected = false
+    }
 </script>
 
-<button class="aspect-[287/400] w-40 -mb-60 transition-transform duration-300 transform hover:translate-y-8 shadow-md">
-    <img class="w-[100%] h-[100%]" src="card-back.png" alt="card">
+<button {onclick} class="aspect-[287/400] w-40 -mb-60 transition-transform duration-300 transform shadow-md {selected ? "translate-y-96" : ""}">
+    <img class="w-[100%] h-[100%]" src="{selected ? "clubs-2.png" : "card-back.png"}" alt="card">
 </button>
--- src/lib/CardPile.svelte
+++ src/lib/CardPile.svelte
@@ -1,14 +1,29 @@
-<script>
-
+<script lang="ts">
     import Card from "./Card.svelte";
 
+    let cards = $state([
+        { number: 1, selected: false },
+        { number: 2, selected: false },
+        { number: 3, selected: false },
+        { number: 4, selected: false },
+        { number: 5, selected: false },
+        { number: 6, selected: false }
+    ])
+
+    function onclickcapture() {
+        for (let card of cards) {
+            if (card.selected) {
+                return
+            }
+        }
+
+        let card = cards[Math.floor(Math.random() * 6)]
+        card.selected = true
+    }
 </script>
 
-<button class="flex flex-col">
-    <Card></Card>
-    <Card></Card>
-    <Card></Card>
-    <Card></Card>
-    <Card></Card>
-    <Card></Card>
+<button {onclickcapture} class="flex flex-col">
+     {#each cards as card (card.number)}
+        <Card number={card.number} bind:selected={card.selected}/>
+     {/each}
 </button>
