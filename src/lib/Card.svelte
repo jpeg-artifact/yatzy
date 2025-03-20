83e8b4e0117c919077457efeb036b4022ec14cd8
JPEG <139462407+jpeg-artifact@users.noreply.github.com>
2025-03-20 07:39:49 +0100

    feat: add card number showing

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -4,8 +4,32 @@
     function onclick() {
         selected = false
     }
+
+    function getcardimg() {
+        if (number === 1) {
+            return ["clubs-a.png", "hearts-a.png", "diamonds-a.png", "spades-a.png"][Math.floor(Math.random() * 4)]
+        }
+        else if (number === 2) {
+            return ["clubs-2.png", "hearts-2.png", "diamonds-2.png", "spades-2.png"][Math.floor(Math.random() * 4)]
+        }
+        else if (number === 3) {
+            return ["clubs-3.png", "hearts-3.png", "diamonds-3.png", "spades-3.png"][Math.floor(Math.random() * 4)]
+        }
+        else if (number === 4) {
+            return ["clubs-4.png", "hearts-4.png", "diamonds-4.png", "spades-4.png"][Math.floor(Math.random() * 4)]
+        }
+        else if (number === 5) {
+            return ["clubs-5.png", "hearts-5.png", "diamonds-5.png", "spades-5.png"][Math.floor(Math.random() * 4)]
+        }
+        else if (number === 6) {
+            return ["clubs-6.png", "hearts-6.png", "diamonds-6.png", "spades-6.png"][Math.floor(Math.random() * 4)]
+        }
+        else {
+            return "card-back.png"
+        }
+    }
 </script>
 
 <button {onclick} class="aspect-[287/400] w-40 -mb-60 transition-transform duration-300 transform shadow-md {selected ? "translate-y-96" : ""}">
-    <img class="w-[100%] h-[100%]" src="{selected ? "clubs-2.png" : "card-back.png"}" alt="card">
+    <img class="w-[100%] h-[100%]" src="{selected ? getcardimg() : "card-back.png"}" alt={number}>
 </button>
