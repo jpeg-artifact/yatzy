32f83e048c058caf9f576c9a9dfe088d850d80cf
JPEG <139462407+jpeg-artifact@users.noreply.github.com>
2025-03-23 11:57:56 +0100

    feat: add Player

---
+++ src/lib/Player.svelte
@@ -0,0 +1,199 @@
+<style>
+    td {
+        border-style: var(--tw-border-style);
+        border-width: 2px;
+        padding: 4px;
+        padding-left: 8px;
+        padding-right: 32px;
+        min-width: 128px;
+    }
+    h1 {
+        font-weight: bold;
+    }
+</style>
+
+<div class="bg-yellow-50 p-4 float-right shadow-md mt-8">
+    <table class="bg-yellow-50 border-4">
+        <tbody>
+            <tr class="border-4">
+                <td>
+                    <h1 class="text-1xl">SPELARE</h1> 
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>
+                    Ettor 
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Tvåor
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Treor
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Fyror
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Femmor
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Sexor
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+        </tbody>
+        <tbody>
+            <tr class="border-4">
+                <td>
+                    <h1 class="text-1xl">SUMMA</h1> 
+                </td>
+                <td>
+
+                </td>
+                <td>
+                    <p class="text-xs">63 eller mer = bonus</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Bonus 
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    <p class="text-xs">50 poäng</p>
+                </td>
+            </tr>
+            <tr>
+                <td> 
+                    Ett par
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Två par
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Tretal
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Fyrtal
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Kåk
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Liten stege
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Stor stege
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Chans
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td> 
+                    Yatzy
+                </td>
+                <td>
+                    
+                </td>
+                <td>
+                    <p class="text-xs">50 poäng</p>
+                </td>
+            </tr>
+            <tr class="border-4">
+                <td>
+                    <h1 class="text-1xl">SUMMA</h1> 
+                </td>
+                <td>
+                    
+                </td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import Attribution from "$lib/Attribution.svelte";
     import CardPile from "$lib/CardPile.svelte";
     import { CardPileClass } from "$lib/CardPileClass.svelte";
+    import Player from "$lib/Player.svelte";
 
     let cardPiles = $state([
         new CardPileClass(),
@@ -12,10 +13,13 @@
     ]);
 </script>
 
-<div class="flex gap-8 ml-8">
-    {#each cardPiles as cardPile (cardPile)}
-        <CardPile {cardPile}></CardPile>
-    {/each}
+<div class="flex gap-16">
+    <div class="flex gap-8 ml-8">
+        {#each cardPiles as cardPile (cardPile)}
+            <CardPile {cardPile}></CardPile>
+        {/each}
+    </div>
+    <Player></Player>
 </div>
 
 <Attribution></Attribution>
